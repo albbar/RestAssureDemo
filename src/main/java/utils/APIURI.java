f4f921ae2b80f88ca8bdcf39df63f834e57ec0ff
alberto <alcabarme@gmail.com>
2022-04-23 04:55:14 -0500

    first commit

---
+++ src/main/java/utils/APIURI.java
@@ -0,0 +1,14 @@
+package utils;
+
+public enum APIURI {
+    googleMapAPI("https://rahulshettyacademy.com"),
+    calculatorAPI("http://www.dneonline.com/");
+
+    private String uri;
+
+    private APIURI(String uri){ this.uri = uri;}
+
+    public String getURI(){
+        return this.uri;
+    }
+}
